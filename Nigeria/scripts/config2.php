81ab415f927640d472ded8a7f756ca413bc1ab88
seyi <adelegan.seyi@zodml.org>
2014-12-15 01:18:30 -0500

    fourth commit

---
+++ Nigeria/scripts/config2.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$db_host = "localhost"; //your host Database adress quite often localhost
+$db_username = "zodmlorg"; //your username
+$db_pass = "npy7T?jnQ!tq"; //your password
+$db_name = "zodmlorg_books"; //your database name
+@mysql_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
+@mysql_select_db("$db_name") or die ("no database");
+
+
+?>
---
+++ Nigeria/scripts/discover_nigeria.php
@@ -0,0 +1,215 @@
+<?php 
+								ini_set("display_errors", "off");
+								include_once("scripts/config.php");
+								$sql2 = mysql_query("SELECT * FROM contentsupdate WHERE id=16");
+								while($row2 = mysql_fetch_array($sql2)){
+								$id2 = $row2["id"];
+								$content2 = $row2["contents"];
+								$title2 = $row2["title"];
+								$tags = $row2["metatags"];
+								$description = $row2["metadescription"];
+								$metatitle = $row2["metatitle"];
+								}
+					 			?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="<?php echo $tags; ?>" />
+<meta name="title" content="<?php echo $metatitle; ?>" />
+<meta name="description" content="<?php echo $description; ?>" />
+<meta name="googlebot" content="index, follow" />
+<meta name="robots" content="index, follow" />
+<meta name="Revisit-After" content="7 days" />
+<meta name="city" content="Lagos" />
+<meta name="country" content="Nigeria(NGA)" />
+<meta name="state" content="LA" />
+<meta name="zip code" content="101233" />
+<meta name="subject" content="Online Library in Nigeria, Learning Resources" />
+<meta name="author" content="Zaccheus Onumba Dibiaezue Memorial Libraries" />
+<meta name="copyright" content="Zaccheus Onumba Dibiaezue Memorial Libraries" />
+<title>ZODML | Discover Nigeria | Facts about Nigeria</title>
+<link href="../css/styles.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="../css/menu.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="../css/bottom.css" type="text/css" rel="stylesheet" media="screen"/>
+<link href="../images/favicon.ico" rel="shortcut icon" />
+<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js"></script>-->
+<script type='text/javascript' src="../js/pixreg1.js"></script>
+<script type='text/javascript' src="../js/pixreg2.js"></script>
+<script type='text/javascript' src='../js/jquery.hoverIntent.minified.js'></script>
+<script type='text/javascript' src='../js/jquery.dcmegamenu.1.2.js'></script>
+<script type="text/javascript" src="../js/jquery.pikachoose.js"></script>
+<script type="text/javascript" src="../js/search.js"></script>
+<script type="text/javascript" src="../js/contact.js"></script>
+<script type="text/javascript">
+$(document).ready(function($){
+	$('#mega-menu-tut').dcMegaMenu({
+		rowItems: '3',
+		speed: 'fast'
+	});
+});
+</script>
+
+
+<script type="text/javascript">
+$(function(){
+	$("input:radio[name='search']").click(function(){	
+		var criteria=$("input:radio[name='search']:checked").val();
+		if(criteria=="Other")
+		$("#select_box").enable();
+	});
+});
+
+</script>
+
+<!--GOOGLE PAGE-TRACK CODE-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33455239-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!--END OF GOOGLE PAGE-TRACK CODE-->
+<!--KISS METRICS PAGE-TRACK CODE-->
+<script type="text/javascript">
+  var _kmq = _kmq || [];
+  var _kmk = _kmk || '0e0abe024abceaf3faa5a718c5f8b02eab21a41d';
+  function _kms(u){
+    setTimeout(function(){
+      var d = document, f = d.getElementsByTagName('script')[0],
+      s = d.createElement('script');
+      s.type = 'text/javascript'; s.async = true; s.src = u;
+      f.parentNode.insertBefore(s, f);
+    }, 1);
+  }
+  _kms('//i.kissmetrics.com/i.js');
+  _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+</script>
+<!--END OF KISS METRICS PAGE-TRACK CODE-->
+<SCRIPT TYPE="text/javascript">
+<!--
+//Disable right click script
+//visit http://www.rainbow.arch.scriptmania.com/scripts/
+var message="Sorry, right-click has been disabled";
+///////////////////////////////////
+function clickIE() {if (document.all) {(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false")
+// -->
+</SCRIPT> 
+</head>
+
+<body>
+
+<!-- Start of StatCounter Code for Dreamweaver -->
+<script type="text/javascript">
+var sc_project=9025802; 
+var sc_invisible=0; 
+var sc_security="701b465a"; 
+var sc_text=2; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="hits counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9025802/0/701b465a/0/"
+alt="hits counter"></a></div></noscript>
+<!-- End of StatCounter Code for Dreamweaver -->
+
+<!--BODY HOLDER-->
+<div id="body_hold">
+<!--<div style="float:left; height:580px; width:100px; margin:118px 0px 0px 10px;"><img src="../images/christmasdeco2.png" width="100" height="573" /></div>-->
+<!--HEADER-->
+                <div id="header">
+                <a href="http://zodml.org"><div id="logo-link"></div></a>
+                
+                <!--<div id="subheader_hold">-->
+                <div id="subheader">
+                <br />
+                
+                	<?php include("search_bar.php"); ?>
+                </div>
+                <!--</div>-->	
+                </div>
+<!--END OF HEADER-->
+<!--<div style="float:right; width:100px; height:580px; margin:0px 10px 0px 0px;"><img src="../images/christmasdeco1.png" width="100" height="573" /></div>-->
+<!--MENU-->
+			<div id="menu_ba">
+            	<?php include("menu.php");?>
+            </div>
+<!--END OF MENU-->
+
+<!--CONTENT-->
+<div id="content_holder">
+
+				<?php echo $content2; ?>
+                    
+
+    
+
+    
+                
+</div>
+<!--END OF CONTENT-->
+
+<!--FOOTER-->
+<div id="footer_new_hold">
+<?php include("footer.php"); ?>
+</div>
+<!--END OF FOOTER-->
+
+</div>
+<!--END OF THE BODY HOLDER-->
+<form id="contactForm" action="../processForm.php" method="post">
+
+  <h2>Send us an email...</h2>
+
+  <ul>
+
+    <li>
+      <label for="senderName" class="label">Your Name</label>
+      <input type="text" name="senderName" id="senderName" placeholder="Please type your name" required maxlength="40" class="input" />
+    </li>
+
+    <li>
+      <label for="senderEmail" class="label">Your Email Address</label>
+      <input type="email" name="senderEmail" id="senderEmail" placeholder="Please type your email address" required maxlength="50" class="input" />
+    </li>
+
+    <li>
+      <label for="message" style="padding-top: .5em;" class="label">Your Message</label>
+      <textarea name="message" id="message" placeholder="Please type your message" required cols="80" rows="10" maxlength="10000" class="textarea"></textarea>
+    </li>
+
+  </ul>
+
+  <div id="formButtons">
+    <input type="submit" id="sendMessage" name="sendMessage" value="Send Email" />
+    <input type="button" id="cancel" name="cancel" value="Cancel" />
+  </div>
+
+</form>
+
+<div id="sendingMessage" class="statusMessage"><p>Sending your message. Please wait...</p></div>
+<div id="successMessage" class="statusMessage"><p>Thanks for sending your message! We'll get back to you shortly.</p></div>
+<div id="failureMessage" class="statusMessage"><p>There was a problem sending your message. Please try again.</p></div>
+<div id="incompleteMessage" class="statusMessage"><p>Please complete all the fields in the form before sending.</p></div>
+</body>
+</html>
