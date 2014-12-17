48933b4f1be118b7f28eeeacbd32696aebf4cf3c
seyi <adelegan.seyi@zodml.org>
2014-12-17 05:46:15 -0500

    commit after update2 gitignore

--- precurio/application/news/models/vo/RssNews.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class RssNews extends News {
-
-}
-
-?>
--- precurio/application/visitor/models/VisitorStrings.php
+++
@@ -1,9 +0,0 @@
-<?php
-class VisitorStrings
-{
-	const USER = 0; // Getting visits for staff view
-	const RECEPTIONIST = 1; // Getting visits for a receptionist view
-	const ADMIN = 2; // Getting visits for admin view
-}
-
-?>
