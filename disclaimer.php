ce58f3cd4b8efb45264f61c898aca8533a47473c
seyi <adelegan.seyi@zodml.org>
2015-01-19 11:59:29 +0100

    rough commit

---
+++ disclaimer.php
@@ -0,0 +1,133 @@
+<?php  header( 'Location: http://zodml.org/using-website-and-privacy-policy' ) ;  ?>
+<?php
+ini_set("display_errors", "off");
+include_once("Nigeria/scripts/config.php");
+$sql2 = mysql_query("SELECT * FROM contentsupdate WHERE id=71");
+while($row2 = mysql_fetch_array($sql2)){
+$id2 = $row2["id"];
+$content2 = $row2["contents"];
+$title2 = $row2["title"];
+$tags = $row2["metatags"];
+$description = $row2["metadescription"];
+$metatitle = $row2["metatitle"];
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="<?php echo $tags; ?>" />
+<meta name="title" content="<?php echo $metatitle; ?>" />
+<meta name="description" content="<?php echo $description; ?>" />
+<meta name="googlebot" content="index, follow" />
+<meta name="robots" content="index, follow" />
+<meta name="Revisit-After" content="7 days" />
+<meta name="city" content="Lagos" />
+<meta name="country" content="Nigeria(NGA)" />
+<meta name="state" content="LA" />
+<meta name="zip code" content="101233" />
+<meta name="subject" content="Online Library in Nigeria, Learning Resources" />
+<meta name="author" content="Zaccheus Onumba Dibiaezue Memorial Libraries" />
+<meta name="copyright" content="Zaccheus Onumba Dibiaezue Memorial Libraries" />
+<title>ZODML | Disclaimer </title>
+<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="css/menu.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="css/bottom.css" type="text/css" rel="stylesheet" media="screen"/>
+<link href="images/favicon.ico" rel="shortcut icon" />
+<!--<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js"></script>-->
+<script type='text/javascript' src="js/pixreg1.js"></script>
+<script type='text/javascript' src="js/pixreg2.js"></script>
+<script type='text/javascript' src='js/jquery.hoverIntent.minified.js'></script>
+<script type='text/javascript' src='js/jquery.dcmegamenu.1.2.js'></script>
+<!--<script type="text/javascript" src="js/pack.js"></script>-->
+<script type="text/javascript" src="js/search.js"></script>
+<script type="text/javascript">
+$(document).ready(function($){
+	$('#mega-menu-tut').dcMegaMenu({
+		rowItems: '3',
+		speed: 'fast'
+	});
+});
+</script>
+<script language="javascript">
+  function toggleDiv(divid){
+    if(document.getElementById(divid).style.display == 'none'){
+      document.getElementById(divid).style.display = 'block';
+    }else{
+      document.getElementById(divid).style.display = 'none';
+    }
+  }
+</script>
+
+
+<script type="text/javascript">
+$(function(){
+	$("input:radio[name='search']").click(function(){	
+		var criteria=$("input:radio[name='search']:checked").val();
+		if(criteria=="Other")
+		$("#select_box").enable();
+	});
+});
+
+</script>
+
+<!--GOOGLE PAGE-TRACK CODE-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33247984-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!--END OF GOOGLE PAGE-TRACK CODE-->
+
+</head>
+
+<body>
+<!--BODY HOLDER-->
+<div id="body_hold">
+			<!--HEADER-->
+                 <div id="header">
+                 <a href="http://zodml.org"><div id="logo-link"></div></a>
+                 
+                <div id="subheader">
+               <br />
+                	<?php include("search_bar.php"); ?>
+                </div>	
+                </div>
+		<!--END OF HEADER-->
+
+<!--MENU-->
+			<div id="menu_ba2">
+            	<?php include("menu.php");?>
+            </div>
+<!--END OF MENU-->
+
+<!--CONTENT HOLDER-->
+<div id="content_holder">
+                		
+                        <!--CONTENT LAYER 1-->
+                        <div id="aboutus_content_layer1"> <font color="#EC9F00">Disclaimer</font></div>
+                        <!--END OF CONTENT LAYER 1-->
+                        <!--CONTENT LAYER 2-->
+    <?php echo $content2; ?>
+                       <!--END OF CONTENT LAYER 2-->
+          
+			</div>
+<!--END OF CONTENT HOLDER-->
+
+<!--FOOTER-->
+<div id="footer_new_hold">
+<?php include("footer.php"); ?>
+</div>
+<!--END OF FOOTER-->
+
+</div>
+<!--END OF THE BODY HOLDER-->
+</body>
+</html>
